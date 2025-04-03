7874bb2d71ba3535e5d99ad764f416fa220f0469
DaniSoler09 <soler092811@gmail.com>
2025-04-03 17:18:39 -0500

    backup

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("boton").style.backgroundColor ="#ffffff";
+
+let titulo = document.getElementById("titulo")
+
+let otroBoton = document.getElementById("cambio")
+
+otroBoton.addEventListener("click", function(){
+    titulo.innerText = "Maria Soler"
+});
